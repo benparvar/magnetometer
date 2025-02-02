8ef86424bdd616c23346955643cddc80adf407c5
Alan Alencar da Silva <benparvar@users.noreply.github.com>
2025-02-02 10:33:00 -0300

    init

--- src/sketch/sketch/sketch.ino
+++ src/sketch/sketch/sketch.ino
@@ -1,19 +1,30 @@
 #include <Wire.h>
+#include <I2Cdev.h>
+#include <HMC5883L.h>
 
 #define ADDRESS_HMC5883 0x0D // Encontrado na listagem do i2c
 #define SDA_SENSOR 21
 #define SCL_SENSOR 22
 
+HMC5883L mag = HMC5883L(ADDRESS_HMC5883);
+
 bool isSensorAvailable = false;
 
 void configureSensor() {
   Serial.println("configureSensor");
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
   Wire.begin();
 
-}
-
-void configureLCD() {
-
+   // initialize device
+  Serial.println("Initializing I2C devices...");
+  mag.
+  mag.initialize();
+  
+  // verify connection
+  Serial.println("Testing device connections...");
+  isSensorAvailable = mag.testConnection();
+  Serial.println(isSensorAvailable ? "HMC5883L connection successful": "HMC5883L connection failed");
 }
 
 void setup() {
@@ -25,7 +36,7 @@ void setup() {
 }
 
 void readSensor() {
-  Serial.println("readSensor");
+  //Serial.println("readSensor");
  
 }
 
